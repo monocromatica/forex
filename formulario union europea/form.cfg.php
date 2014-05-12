4c1a1df622957519c867d60e70e5055478ac33ba
monocromatica <hola@monocromatica.mx>
2014-05-12 13:17:46 -0500

    Cambio de metodo de compra

--- formulario union europea/form.cfg.php
+++ formulario union europea/form.cfg.php
@@ -46,7 +46,7 @@
 			"custom" : 
 			{
 				"body" : "<!DOCTYPE html>\n<html dir=\"ltr\" lang=\"en\">\n<head><title>You got mail!</title></head>\n<body style=\"background-color: #f9f9f9; padding-left: 11%; padding-top: 7%; padding-right: 20px; max-width: 700px; font-family: Helvetica, Arial;\">\n<style type=\"text/css\">\nbody {background-color: #f9f9f9;padding-left: 11%; padding-top: 7%; padding-right: 2%;max-width:700px;font-family: Helvetica, Arial;}\np{font-size: 12px; color: #666666;}\nh1{font-size: 60px !important;color: #cccccc !important;margin:0px;}\nh2{font-size: 28px !important;color: #666666 ! important;margin: 0px; border-bottom: 1px dotted #00A2FF; padding-bottom:3px;}\ntable{width:80%;}\ntd {font-size: 12px !important; line-height: 30px;color: #666666 !important; margin: 0px;border-bottom: 1px solid #e9e9e9;}\ntd:first-child {font-size: 13px !important; font-weight:bold; color: #333 !important; vertical-align:text-top; min-width:10%; padding-right:5px;}\na:link {color:#666666; text-decoration:underline;} a:visited {color:#666666; text-decoration:none;} a:hover {color:#00A2FF;}\nb{font-weight: bold;}\n</style>\n<h1 style=\"font-size: 60px !important; color: #cccccc !important; margin: 0px;\">Hey there,</h1>\n<p style=\"font-size: 12px; color: #666666;\">\nSomeone filled out your form, and here's what they said:\n</p>\n<div>\n[_form_results_]\n<p style=\"font-size: 12px; color: #666666;\">\n<b style=\"font-weight: bold;\">Note: </b> Customizing your email message is as easy as 1-2-3! To get started, edit your form and go into the <b style=\"font-weight: bold;\">Settings &gt; Email Notices Tab</b> and then click on <b style=\"font-weight: bold;\">\"Configure email message\"</b>. Web Form Builder supports HTML and CSS so there is no limit to how snazzy you can design your message.\nFor more info <a href=\"http://coffeecup.com\" style=\"color: #666666; text-decoration: underline;\">go here</a>.\nHope you're enjoying the software!\n</p>\n</div>\n</body>\n</html>\n",
-				"is_present" : true,
+				"is_present" : false,
 				"subject" : "Vendiste un producto[nombre]!"
 			},
 			"from" : "ray@monocromatica.mx",
@@ -73,27 +73,33 @@
 	"payment_settings" : 
 	{
 		"confirmpayment" : "<center>\n<style type=\"text/css\">\n#docContainer table {width:80%; margin-top: 5px; margin-bottom: 5px;}\n#docContainer td {text-align:right; min-width:25%; font-size: 12px !important; line-height: 20px;margin: 0px;border-bottom: 1px solid #e9e9e9; padding-right:5px;}\n#docContainer td:first-child {text-align:left; font-size: 13px !important; font-weight:bold; vertical-align:text-top; min-width:50%;}\n#docContainer th {font-size: 13px !important; font-weight:bold; vertical-align:text-top; text-align:right; padding-right:5px;}\n#docContainer th:first-child {text-align:left;}\n#docContainer tr:first-child {border-bottom-width:2px; border-bottom-style:solid;}\n#docContainer center {margin-bottom:15px;}\n#docContainer form input { margin:5px; }\n#docContainer #fb_confirm_inline { margin:5px; text-align:center;}\n#docContainer #fb_confirm_inline>center h2 { }\n#docContainer #fb_confirm_inline>center p { margin:5px; }\n#docContainer #fb_confirm_inline>center a { }\n#docContainer #fb_confirm_inline input { border:none; color:transparent; font-size:0px; background-color: transparent; background-repat: no-repeat; }\n#docContainer #fb_paypalwps { background: url('https://coffeecupimages.s3.amazonaws.com/paypal.gif');background-repeat:no-repeat; width:145px; height:42px; }\n#docContainer #fb_googlepay { background: url('https://coffeecupimages.s3.amazonaws.com/googlecheckout.gif'); background-repeat:no-repeat; width:168px; height:44px; }\n#docContainer #fb_authnet { background: url('https://coffeecupimages.s3.amazonaws.com/authnet.gif'); background-repeat:no-repeat; width:135px; height:38px; }\n#docContainer #fb_2checkout { background: url('https://coffeecupimages.s3.amazonaws.com/2co.png'); background-repeat:no-repeat; width:210px; height:44px; }\n#docContainer #fb_invoice { background: url('https://coffeecupimages.s3.amazonaws.com/btn_email.png'); background-repeat:no-repeat; width:102px; height:31px; }\n#docContainer #fb_invoice:hover { background: url('https://coffeecupimages.s3.amazonaws.com/btn_email_hov.png'); }\n#docContainer #fb_goback { color: inherit; }\n</style>\n[_cart_summary_]\n<h2>Almost done! </h2>\n<p>Your order will not be processed until you click the payment button below.</p>\n<a id=\"fb_goback\"href=\"?action=back\">Back to form</a></center>",
-		"currencysymbol" : "$",
+		"currencysymbol" : "€",
 		"decimals" : 2,
-		"fixedprice" : "000",
+		"fixedprice" : "100000",
 		"invoicelabel" : "",
 		"is_present" : false,
 		"paymenttype" : "redirect",
-		"shopcurrency" : "USD",
+		"paypalwps" : 
+		{
+			"business" : "",
+			"enabled" : true,
+			"pdt_token" : ""
+		},
+		"shopcurrency" : "EUR",
 		"usecustomsymbol" : false
 	},
 	"redirect_settings" : 
 	{
 		"confirmpage" : "<!DOCTYPE html>\n<html dir=\"ltr\" lang=\"en\">\n<head>\n<title>Success!</title>\n<meta charset=\"utf-8\">\n<style type=\"text/css\">\nbody {background: #f9f9f9;padding-left: 11%;padding-top: 7%; padding-right: 2%;max-width:700px;font-family: Helvetica, Arial;}\ntable{width:80%;}\np{font-size: 16px;font-weight: bold;color: #666;}\nh1{font-size: 60px !important;color: #ccc !important;margin:0px;}\nh2{font-size: 28px !important;color: #666 !important;margin: 0px; border-bottom: 1px dotted #00A2FF; padding-bottom:3px;}\nh3{font-size: 16px; color: #a1a1a1; border-top: 1px dotted #00A2FF; padding-top:1.7%; font-weight: bold;}\nh3 span{color: #ccc;}\ntd {font-size: 12px !important; line-height: 30px;  color: #666 !important; margin: 0px;border-bottom: 1px solid #e9e9e9;}\ntd:first-child {font-size: 13px !important; font-weight:bold; color: #333 !important; vertical-align:text-top; min-width:50%; padding-right:5px;}\na:link {color:#666; text-decoration:none;} a:visited {color:#666; text-decoration:none;} a:hover {color:#00A2FF;}\n</style>\n</head>\n<body>\n<h1>Thanks! </h1>\n<h2>The form is on its way.</h2>\n<p>Here&rsquo;s what was sent:</p>\n<div>[_form_results_]</div>\n<!-- link back to your Home Page -->\n<h3>Let&rsquo;s go <span> <a target=\"_blank\" href=\"http://www.coffeecup.com\">Back Home</a></span></h3>\n</body>\n</html>\n",
-		"gotopage" : "http://www.monocromatica.mx",
+		"gotopage" : "felicidades.html",
 		"inline" : "<center>\n<style type=\"text/css\">\n#docContainer table {margin-top: 30px; margin-bottom: 30px; width:80%;}\n#docContainer td {font-size: 12px !important; line-height: 30px;color: #666666 !important; margin: 0px;border-bottom: 1px solid #e9e9e9;}\n#docContainer td:first-child {font-size: 13px !important; font-weight:bold; color: #333 !important; vertical-align:text-top; min-width:50%; padding-right:5px;}\n</style>\n[_form_results_]\n<h2>GRACIAS!</h2><br/>\n<p>Tu compra ha sido exitosa.</p>\n</center>",
-		"type" : "inline"
+		"type" : "gotopage"
 	},
 	"uid" : "1d85a749e91583f0229a440470de25bb",
 	"validation_report" : "in_line"
 },
-"rules":{"nombre":{"label":"Nombre","fieldtype":"text","required":true,"messages":"Por favor inserta tu nombre"},"apellido":{"label":"Apellido","fieldtype":"text","required":true,"messages":"Por favor inserta tu nombre"},"email":{"email":true,"label":"Email","fieldtype":"email","required":true,"messages":"Por favor inserta un correo de email v&aacute;lido"},"telefono":{"label":"Tel&eacute;fono","fieldtype":"text"},"direccion":{"label":"Direcci&oacute;n","fieldtype":"text"},"ciudad":{"label":"Ciudad","fieldtype":"text","required":true},"estado":{"label":"Estado","fieldtype":"text","required":true},"pais":{"label":"Pa&iacute;s","fieldtype":"text","required":true},"zis":{"fieldtype":"text","label":"Codigo Postal"},"datos-fiscales":{"fieldtype":"textarea","maxlength":"10000","label":"Datos F&iacute;scales"},"factura":{"checkbox":true,"number_required":0,"label":"&iquest;Requiere factura?","fieldtype":"checkbox","required":false,"values":["Si, necesito factura para mi negocio"]},"terminos":{"checkbox":true,"number_required":1,"label":"T&eacute;rminos y condiciones de uso","fieldtype":"checkbox","required":true,"values":["Acepto los teminos y condiciones de uso"]}},
-"payment_rules":{"factura":{},"terminos":{}},
+"rules":{"nombre":{"label":"Nombre","fieldtype":"text","required":true,"messages":"Por favor inserta tu nombre"},"apellido":{"label":"Apellido","fieldtype":"text","required":true,"messages":"Por favor inserta tu nombre"},"email":{"email":true,"label":"Email","fieldtype":"email","required":true,"messages":"Por favor inserta un correo de email v&aacute;lido"},"telefono":{"label":"Tel&eacute;fono","fieldtype":"text"},"direccion":{"label":"Direcci&oacute;n","fieldtype":"text"},"ciudad":{"label":"Ciudad","fieldtype":"text","required":true},"estado":{"label":"Estado","fieldtype":"text","required":true},"select19":{"label":"Pa&iacute;s","fieldtype":"dropdown","values":["Opción 1","Opción 2","Opción 3"]},"zis":{"fieldtype":"text","label":"Codigo Postal"},"datos-fiscales":{"fieldtype":"textarea","maxlength":"10000","label":"Datos F&iacute;scales"},"factura":{"checkbox":true,"number_required":0,"label":"&iquest;Requiere factura?","fieldtype":"checkbox","required":false,"values":["Si, necesito factura para mi negocio"]},"terminos":{"checkbox":true,"number_required":1,"label":"T&eacute;rminos y condiciones de uso","fieldtype":"checkbox","required":true,"values":["Acepto los teminos y condiciones de uso"]}},
+"payment_rules":{"select19":{},"factura":{},"terminos":{}},
 "conditional_rules":{"datos-fiscales":{"element":{"name":"factura","operator":"is","value":"Si, necesito factura para mi negocio"}}},
 "application_version":"Web Form Builder (OSX), build 2.3.5217"
 }
